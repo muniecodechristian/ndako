d7acfc9c7bd0da60f9dcc7da2a7e8665d6b46b6b
muniecodechristian <muniecodechristian@gmail.com>
2025-09-09 01:36:53 +0200

    Update post.controller.js

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -172,7 +172,7 @@ module.exports.searchPosts = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: "Erreur serveur", error: err.message });
   }
-};
+
 
     res.status(200).json(posts);
   } catch (err) {
