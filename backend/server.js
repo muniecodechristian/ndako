1ddffbe7b907f880bbb185aa64c5b4dff988a100
muniecodechristian <muniecodechristian@gmail.com>
2025-09-11 14:00:17 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS configuration pour permettre l'envoi de cookies
 app.use(cors({
-   origin: true, // Remplace avec ton frontend en prod
+   origin:' http://localhost:5173', // Remplace avec ton frontend en prod
   credentials: true
 }));
 app.use('/uploads', express.static('uploads'));
